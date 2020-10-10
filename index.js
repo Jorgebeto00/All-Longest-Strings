06cc9a87140e71c5963127d1037c3b2581c654be
Jorge R <jorgebeto00@gmail.com>
2020-10-10 01:18:19 +0000

    final

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ function allLongestStrings(inputArray) {
 	let arrStr = inputArray;
 	let arrLen = [];
 	let arrPos = [];
+	let arrFinal = [];
 	
 
 	for(let i = 0; i < arrStr.length; i++){
-		//console.log(i);
 		arrPos.push(i);
 		arrLen.push(arrStr[i].length);
-		
 	}
-	let max = Math.max(arrLen);
-	return arrLen;
+
+	let max = Math.max(...arrLen);
+
+	for(let x = 0; x < arrStr.length; x++){
+		if(arrStr[x].length === max){
+			arrFinal.push(arrStr[x]);
+		}
+	}
+	
+	return arrFinal;
 }
 
 allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]);
